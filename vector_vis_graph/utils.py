bcfed8cad1990fe11126b22f65039d5e12e612b0
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-12-25 01:48:37 +0300

    feat: convert from `torch` to `numpy/numba`
    The visibility graph generation highly depends on `for`-loops, and
    data-specific accesses with conditional statements. This makes it
    unsuitable for GPU acceleration and mappings like `torch.vmap()`.
    
    Therefore, to increase the performance, the code is converted from
    `torch` to `numpy` with `numba` JIT compilation. This conversion highly
    improves the performance of the code, even with the overhead of
    GPU-to-CPU-to-GPU data transfer.

--- vector_vis_graph/utils.py
+++ vector_vis_graph/utils.py
@@ -1,16 +1,14 @@
-import torch
+import numpy as np
 
 
-def project_onto_vector(
-    from_tensor: torch.Tensor,
-    onto_vector: torch.Tensor,
-) -> torch.Tensor:
-    return torch.linalg.vecdot(from_tensor, onto_vector) / onto_vector.norm()
+def project_onto_vector(from_matrix: np.ndarray, onto_vector: np.ndarray) -> np.ndarray:
+    return np.dot(from_matrix, onto_vector) / np.linalg.norm(onto_vector)
 
 
-def project_onto_tensor(
-    from_tensor: torch.Tensor,
-    onto_tensor: torch.Tensor,
-) -> torch.Tensor:
-    batched = torch.vmap(project_onto_vector, in_dims=(None, 0))
-    return batched(from_tensor, onto_tensor)
+def project_onto_matrix(from_matrix: np.ndarray, onto_matrix: np.ndarray) -> np.ndarray:
+    vectorized_project = np.vectorize(
+        project_onto_vector,
+        excluded="from_matrix",
+        signature="(m,n),(n)->(m)",
+    )
+    return vectorized_project(from_matrix, onto_matrix)
--- vector_vis_graph/vvg.py
+++ vector_vis_graph/vvg.py
@@ -1,33 +1,36 @@
 from typing import Optional
 
-import torch
+import numpy as np
+from numba import njit
 
-from vector_vis_graph.utils import project_onto_tensor
+from vector_vis_graph.utils import project_onto_matrix
 
 
+@njit(cache=True)
 def calculate_weight(
-    vector_a: torch.Tensor,
-    vector_b: torch.Tensor,
-    time_a: torch.Tensor,
-    time_b: torch.Tensor,
+    vector_a: np.ndarray,
+    vector_b: np.ndarray,
+    time_a: float,
+    time_b: float,
     weighted: bool = False,
-) -> torch.Tensor:
+) -> float:
     if weighted:
-        inner = torch.dot(vector_a, vector_b) / (vector_a.norm() * vector_b.norm())
-        return inner / torch.abs(time_b - time_a)
+        magnitude = float(np.linalg.norm(vector_a)) * float(np.linalg.norm(vector_b))
+        inner = np.dot(vector_a, vector_b) / magnitude
+        return inner / np.abs(time_b - time_a)
     else:
-        return torch.tensor(1.0)
+        return 1.0
 
 
+@njit(cache=True)
 def _natural_vvg_loop(
-    multivariate_tensor: torch.Tensor,
-    timeline: torch.Tensor,
-    projections: torch.Tensor,
+    multivariate_tensor: np.ndarray,
+    timeline: np.ndarray,
+    projections: np.ndarray,
     weighted: bool,
-    device: torch.device,
-) -> torch.Tensor:
-    time_length = timeline.size(0)
-    vvg_adjacency = torch.zeros(time_length, time_length, device=device)
+) -> np.ndarray:
+    time_length = timeline.shape[0]
+    vvg_adjacency = np.zeros((time_length, time_length))
     for a in range(time_length - 1):
         t_a = timeline[a]
         x_aa = projections[a, a]
@@ -39,10 +42,10 @@ def _natural_vvg_loop(
             x_acs = projections[a, a + 1 : b]
             t_cs = timeline[a + 1 : b]
 
-            lhs = torch.div(x_acs - x_ab, t_b - t_cs)
-            rhs = torch.div(x_aa - x_ab, t_b - t_a)
+            lhs = np.divide(x_acs - x_ab, t_b - t_cs)
+            rhs = np.divide(x_aa - x_ab, t_b - t_a)
 
-            if torch.all(lhs < rhs):
+            if np.all(lhs < rhs):
                 vvg_adjacency[a, b] = calculate_weight(
                     multivariate_tensor[a],
                     multivariate_tensor[b],
@@ -55,30 +58,28 @@ def _natural_vvg_loop(
 
 
 def natural_vvg(
-    multivariate_tensor: torch.Tensor,
+    multivariate_tensor: np.ndarray,
     *,
-    timeline: Optional[torch.Tensor] = None,
+    timeline: Optional[np.ndarray] = None,
     weighted: bool = False,
-    device: Optional[torch.device] = None,
-) -> torch.Tensor:
-    if device is None:
-        device = multivariate_tensor.device
-
+) -> np.ndarray:
     if timeline is None:
-        timeline = torch.arange(multivariate_tensor.size(0), device=device)
+        timeline = np.arange(multivariate_tensor.shape[0])
     elif len(timeline.shape) != 1:
         raise ValueError(f"timeline must be a 1D tensor, got {timeline.shape}")
-    elif multivariate_tensor.size(0) != timeline.size(0):
+    elif multivariate_tensor.shape[0] != timeline.shape[0]:
         raise ValueError(
             "multivariate_tensor and timeline must have the same length, "
-            f"got {multivariate_tensor.size(0)} and {timeline.size(0)}"
+            f"got {multivariate_tensor.shape[0]} and {timeline.shape[0]}"
         )
 
-    projections = project_onto_tensor(multivariate_tensor, multivariate_tensor)
+    if multivariate_tensor.ndim == 1:
+        multivariate_tensor = multivariate_tensor.reshape(-1, 1)
+    projections = project_onto_matrix(multivariate_tensor, multivariate_tensor)
+
     return _natural_vvg_loop(
         multivariate_tensor,
         timeline,
         projections,
         weighted,
-        device,
     )
