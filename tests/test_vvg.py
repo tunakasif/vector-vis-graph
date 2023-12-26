42e048f89afedb43959d0155c71c5d435890c36a
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-12-26 16:05:59 +0300

    fix(dc): remove `divide-and-conquer` version
    Even though the method is faster and correct for 1D case, it was not
    outputting correct results for vector cases. With more experimentation,
    it is deduced that it would not have much advantage over the current
    implementation if implemented correctly. Hence, it is removed.

--- tests/test_vvg.py
+++ tests/test_vvg.py
@@ -1,22 +1,11 @@
-from typing import Any
-
 import numpy as np
-from hypothesis import given, settings  # noqa
+from hypothesis import given, settings
 from hypothesis.strategies import integers
 from ts2vg import NaturalVG
 
-from vector_vis_graph.vvg import natural_vvg, natural_vvg_dc
+from vector_vis_graph.vvg import natural_vvg
 
-
-def compare_adj_with_ts2vg(adj: np.ndarray, X: np.ndarray, *args: Any, **kwargs: Any) -> None:
-    vg = NaturalVG(*args, **kwargs)
-    vg.build(
-        X,
-        xs=kwargs.get("xs", None),
-        only_degrees=kwargs.get("only_degrees", False),
-    )
-    ts2vg_adj = vg.adjacency_matrix().astype(np.float64)
-    assert np.allclose(adj, ts2vg_adj)
+# from vector_vis_graph.vvg import natural_vvg, natural_vvg_ts2vg
 
 
 @settings(deadline=5000)
@@ -30,14 +19,6 @@ def test_natural_vvg_1d(time_length: int) -> None:
     vg = NaturalVG(directed="left_to_right")
     vg.build(X)
     ts2vg_adj = vg.adjacency_matrix().astype(np.float64)
-
     assert np.allclose(adj, ts2vg_adj)
 
 
-@settings(deadline=5000)
-@given(integers(min_value=2, max_value=1024))
-def test_natural_vvg_dc_1d(time_length: int) -> None:
-    np.random.seed(0)
-    X = np.random.rand(time_length)
-    adj = natural_vvg_dc(X)
-    compare_adj_with_ts2vg(adj, X, directed="left_to_right")
