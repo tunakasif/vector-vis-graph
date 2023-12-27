572380daaa24c7fda034d6c00010b3204f5093e3
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-12-27 19:21:41 +0300

    test: export `1D` tests to their own file
    Export 1-dimensional sanity checks with `ts2vg`-based
    generation to their own file.

--- tests/test_vvg.py
+++ tests/test_vvg.py
@@ -1,38 +1,9 @@
 import numpy as np
 from hypothesis import given, settings
 from hypothesis.strategies import integers
-from ts2vg import HorizontalVG, NaturalVG
 
 from vector_vis_graph._ts2vg import horizontal_vvg_ts2vg, natural_vvg_ts2vg
 from vector_vis_graph.vvg import horizontal_vvg, natural_vvg
-
-
-@settings(deadline=10000, max_examples=20)
-@given(integers(min_value=2, max_value=1024), integers(min_value=0, max_value=5))
-def test_natural_vvg_1d(time_length: int, penetrable_limit: int) -> None:
-    np.random.seed(0)
-
-    X = np.random.rand(time_length)
-    adj = natural_vvg(X, penetrable_limit=penetrable_limit)
-
-    vg = NaturalVG(directed="left_to_right", penetrable_limit=penetrable_limit)
-    vg.build(X)
-    ts2vg_adj = vg.adjacency_matrix().astype(np.float64)
-    assert np.allclose(adj, ts2vg_adj)
-
-
-@settings(deadline=10000, max_examples=20)
-@given(integers(min_value=2, max_value=1024), integers(min_value=0, max_value=5))
-def test_horizontal_vvg_1d(time_length: int, penetrable_limit: int) -> None:
-    np.random.seed(0)
-
-    X = np.random.rand(time_length)
-    adj = horizontal_vvg(X, penetrable_limit=penetrable_limit)
-
-    vg = HorizontalVG(directed="left_to_right", penetrable_limit=penetrable_limit)
-    vg.build(X)
-    ts2vg_adj = vg.adjacency_matrix().astype(np.float64)
-    assert np.allclose(adj, ts2vg_adj)
 
 
 @settings(deadline=10000, max_examples=20)
