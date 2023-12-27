10c9a2212589db3e06337edefda31cc048467d18
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-12-27 06:04:23 +0300

    test(hvg): add equivalence tests for horizontal vg
    Add equivalence tests for horizontal VVG implementation.

--- tests/test_vvg.py
+++ tests/test_vvg.py
@@ -1,9 +1,14 @@
 import numpy as np
 from hypothesis import given, settings
 from hypothesis.strategies import integers
-from ts2vg import NaturalVG
+from ts2vg import HorizontalVG, NaturalVG
 
-from vector_vis_graph.vvg import natural_vvg, natural_vvg_ts2vg
+from vector_vis_graph.vvg import (
+    horizontal_vvg,
+    horizontal_vvg_ts2vg,
+    natural_vvg,
+    natural_vvg_ts2vg,
+)
 
 
 @settings(deadline=5000, max_examples=20)
@@ -21,10 +26,34 @@ def test_natural_vvg_1d(time_length: int) -> None:
 
 
 @settings(deadline=5000, max_examples=20)
+@given(integers(min_value=2, max_value=1024))
+def test_horizontal_vvg_1d(time_length: int) -> None:
+    np.random.seed(0)
+
+    X = np.random.rand(time_length)
+    adj = horizontal_vvg(X)
+
+    vg = HorizontalVG(directed="left_to_right")
+    vg.build(X)
+    ts2vg_adj = vg.adjacency_matrix().astype(np.float64)
+    assert np.allclose(adj, ts2vg_adj)
+
+
+@settings(deadline=5000, max_examples=20)
 @given(integers(min_value=2, max_value=1024), integers(min_value=1, max_value=1024))
 def test_natural_vvg_equivalence(time_length: int, vec_length: int) -> None:
     np.random.seed(0)
     X = np.random.rand(time_length, vec_length)
     adj = natural_vvg(X)
     adj_ts2vg = natural_vvg_ts2vg(X)
+    assert np.allclose(adj, adj_ts2vg)
+
+
+@settings(deadline=5000, max_examples=20)
+@given(integers(min_value=2, max_value=1024), integers(min_value=1, max_value=1024))
+def test_horizontal_vvg_equivalence(time_length: int, vec_length: int) -> None:
+    np.random.seed(0)
+    X = np.random.rand(time_length, vec_length)
+    adj = horizontal_vvg(X)
+    adj_ts2vg = horizontal_vvg_ts2vg(X)
     assert np.allclose(adj, adj_ts2vg)
