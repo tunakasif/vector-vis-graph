0d98f49547a2dfdc1e2572e2312d11b7ab674c4d
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-12-29 21:22:47 +0300

    feat: allow import from package root
    Allow direct import of most necessary functions and
    weight method enum class from package root.

--- vector_vis_graph/__init__.py
+++ vector_vis_graph/__init__.py
@@ -1,1 +1,11 @@
+from vector_vis_graph.vvg import horizontal_vvg, natural_vvg
+from vector_vis_graph.weight_calculation import WeightMethod
+
 __version__ = "0.6.1"
+
+
+__all__ = [
+    "natural_vvg",
+    "horizontal_vvg",
+    "WeightMethod",
+]
